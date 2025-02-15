0d8c373737b203efc611e9fc0240236948c5c6eb
MitchellWeg <mitchell.w@live.nl>
2025-02-15 19:16:24 +0100

    fix returns

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -29,7 +29,7 @@ def delete_student(student_id):  # noqa: E501
 
     :rtype: object
     """
-    return 200, delete(student_db)
+    return delete(student_db)
 
 
 def get_student_by_id(student_id):  # noqa: E501
@@ -42,4 +42,4 @@ def get_student_by_id(student_id):  # noqa: E501
 
     :rtype: Student
     """
-    return 200, get_by_id(student_id)
+    return get_by_id(student_id)
